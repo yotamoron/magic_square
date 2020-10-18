f9bdbec64c8d5136715949f7922d2bc66b32fcfb
Yotam Oron <yotam@goglobal.travel>
2020-10-18 19:02:37 +0300

    Add TilesShufflerTest

---
+++ MagicSquare/Boards.Test/Tiles/TilesShufflerTest.cs
@@ -0,0 +1,21 @@
+﻿using Boards.Tiles;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Boards.Test.Tiles
+{
+    public class TilesShufflerTest
+    {
+        [Fact]
+        public void ValidateShuffle()
+        {
+            List<Tile> original = Enumerable.Range(0, 24).Select(value => new Tile(value)).ToList();
+            List<Tile> shuffled = new List<Tile>(original);
+            TilesShuffler shuffler = new TilesShuffler();
+
+            shuffler.Shuffle(shuffled);
+            Assert.NotEqual(original, shuffled);
+        }
+    }
+}
