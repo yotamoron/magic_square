35fe52a7adabb1314e4c7b87709bc538184af8f7
Yotam Oron <yotam@goglobal.travel>
2020-10-18 13:54:14 +0300

    Add tests for MovementDisplayNamesResolver

---
+++ MagicSquare/Boards.Test/MovementDisplayNamesResolverTest.cs
@@ -0,0 +1,38 @@
+﻿using Boards.Movements;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Boards.Test
+{
+    public class MovementDisplayNamesResolverTest
+    {
+        [Fact]
+        public void FailResolveUnknownAction()
+        {
+            bool resolved = TryResolve("ThisIsAnIllegelActionName", out Movement movement);
+
+            Assert.False(resolved);
+        }
+
+        [Fact]
+        public void SuccessfullyResolveKnownAction()
+        {
+            bool resolved = TryResolve("D", out Movement movement);
+
+            Assert.True(resolved);
+            Assert.Equal(Movement.DOWN, movement);
+        }
+
+        private bool TryResolve(string action, out Movement movement)
+        {
+            Dictionary<Movement, string> movementDisplayNames = new Dictionary<Movement, string>()
+            {
+                {Movement.DOWN, "D" }
+            };
+            MovementDisplayNamesResolver movementDisplayNamesResolver = new MovementDisplayNamesResolver(movementDisplayNames);
+
+            return movementDisplayNamesResolver.TryResolve(action, out movement);
+
+        }
+    }
+}
