a7f05750b6fa2882181f09543d6a632d9b9e48d0
Yotam Oron <yotam@goglobal.travel>
2020-10-18 17:02:53 +0300

    Add tests for BlankTileIndexFinder

---
+++ MagicSquare/Boards.Test/Tiles/BlankTileIndexFinderTest.cs
@@ -0,0 +1,65 @@
+﻿using Boards.Tiles;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Boards.Test.Tiles
+{
+    public class BlankTileIndexFinderTest
+    {
+        [Fact]
+        public void FailWhenNoBlankTile()
+        {
+            List<Tile> tiles = new List<Tile> { new Tile(0) };
+            bool found = TryFind(tiles, out int dontcare);
+
+            Assert.False(found);
+        }
+
+        [Fact]
+        public void FailWhenMoreThenOneBlankTile()
+        {
+            List<Tile> tiles = new List<Tile> { new Tile(null), new Tile(null) };
+            bool found = TryFind(tiles, out int dontcare);
+
+            Assert.False(found);
+        }
+
+        [Fact]
+        public void SuccessWhenBlankIsFirst()
+        {
+            List<Tile> tiles = new List<Tile> { new Tile(null), new Tile(0) };
+            bool found = TryFind(tiles, out int index);
+
+            Assert.True(found);
+            Assert.Equal(0, index);
+        }
+
+        [Fact]
+        public void SuccessWhenBlankIsLast()
+        {
+            List<Tile> tiles = new List<Tile> { new Tile(0), new Tile(null) };
+            bool found = TryFind(tiles, out int index);
+
+            Assert.True(found);
+            Assert.Equal(1, index);
+        }
+
+        [Fact]
+        public void SuccessWhenBlankNotInExtremities()
+        {
+            List<Tile> tiles = new List<Tile> { new Tile(0), new Tile(null), new Tile(2) };
+            bool found = TryFind(tiles, out int index);
+
+            Assert.True(found);
+            Assert.Equal(1, index);
+        }
+
+        private bool TryFind(List<Tile> tiles, out int index)
+        {
+            BlankTileIndexFinder blankTileIndexFinder = new BlankTileIndexFinder();
+            bool found = blankTileIndexFinder.TryFind(tiles, out index);
+
+            return found;
+        }
+    }
+}
